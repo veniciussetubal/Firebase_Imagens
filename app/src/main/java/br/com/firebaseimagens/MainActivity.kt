83fa2f862cbd0128bc68e78adabd29c5e65630d6
veniciussetubal <venicius.setubal@al.infnet.edu.br>
2020-10-26 08:07:06 -0300

    Initial commit teste

---
+++ app/src/main/java/br/com/firebaseimagens/MainActivity.kt
@@ -0,0 +1,19 @@
+package br.com.firebaseimagens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        var button = findViewById<Button>(R.id.btnVe)
+//        R.id.btnVe
+        btnVe
+
+        
+
+    }
+}
